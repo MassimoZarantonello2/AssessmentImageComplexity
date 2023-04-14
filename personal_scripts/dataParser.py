ed3f9e5de7e29259cf32e7dd3d781e97a7d62b6e
MassimoZarantonello2 <m.zarantonello2@campus.unimib.it>
2023-04-14 23:22:04 +0200

    Trovato nome duplicato, formattati i risultati e i ground truth

--- personal_scripts/dataParser.py
+++ personal_scripts/dataParser.py
@@ -1,20 +1,35 @@
 import re
 
-def parse(path):    
+'''def parse(path):    
     lines = f.readlines()
     for line in lines:
         category,x = line.split("_", 1)
         name,cpl_score = x.split("  ", 1)
-        f2.write(category + " " + name + " " + cpl_score)
-            
+        f2.write(category + " " + name + " " + cpl_score)'''
+        
+def generate_list_dict (path,lista,dict):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            name,cpl_score = line.split("  ", 1)
+            lista.append(name)
+            dict[name] = (cpl_score)
+    return lista,dict
 
-
+                     
             
 if __name__ == "__main__":
     path = "./IC9600/train.txt"
-    path1 = "./IC9600/test.txt"
-    with open("./IC9600/GT_splitted.txt", 'a') as f2:
-        with open(path, 'r') as f:
-            with(path1,'r') as f1:
-                parse(path)
-                parse(path1)
+    path1= "./IC9600/test.txt"
+    GT_dict = {}
+    GT_names = []
+    
+    GT_names,GT_dict = generate_list_dict(path,GT_names,GT_dict)
+    GT_names,GT_dict = generate_list_dict(path1,GT_names,GT_dict)
+    
+    GT_names.sort()
+    with open("./IC9600/GT_IC9600.txt", 'w') as f2:
+        for i in range(len(GT_names)-1):
+            name = GT_names[i]
+            f2.write(name + " " + GT_dict[name])
+    
