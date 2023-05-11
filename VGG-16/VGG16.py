52736babe5d0d30d058193db0a110a4ba7010bf3
Luigi Celona <luigi.celona@disco.unimib.it>
2023-05-11 11:45:43 +0200

    Update VGG16.py
    Bug fixes

--- VGG-16/VGG16.py
+++ VGG-16/VGG16.py
@@ -1,27 +1,30 @@
 import torch.nn as nn
 import torch
 import torchvision.models as models
-import torchvision.models as VGG16_weights
 from torchvision import transforms
 from PIL import Image
 import os
-import math
 
-model = models.vgg16(pretrained=True)
-n_layer =  nn.Sequential(*list(model.children())[:3])
+
+model = models.vgg16(weights='DEFAULT')
+model = nn.Sequential(*list(model.features.children())[:24]) # quarto blocco e non quarto layer
+model.eval() # Fa in modo che dropout e batchnorm siano deterministici (necessario per inferenza)
+model.cuda() # Esegue il modello in GPU, se non disponibile va commentata
+
 
 preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(256),
-    transforms.ToTensor(),
-    #transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                        #std=[0.229, 0.224, 0.225])
+    transforms.ToTensor(), # immagini processate alla loro risoluzione originale
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
 ])
 
+
+@torch.no_grad()
 def get_complexity_score(input_tensor):
-    img_tensor = input_tensor.unsqueeze(0)
-    output = n_layer(img_tensor)
-    return output.mean().data.numpy()
+    img_tensor = input_tensor.cuda().unsqueeze(0)
+    output = model(img_tensor)
+    return output.mean().cpu().numpy()
+
 
 def compute_VGG16_IC9600():
     read_path = './IC9600/images/'
@@ -32,14 +35,13 @@ def compute_VGG16_IC9600():
         for image_name in image_list:
             i+=1
             print(f"Processing image {i}/{len(image_list)}...", end="\r")
-            image = Image.open(read_path + image_name)
+            image = Image.open(read_path + image_name).convert('RGB')
             image_name = image_name.replace(' ','_')
-            if(image.mode != 'RGB'):
-                image = image.convert('RGB')
             image = preprocess(image)
             complexity = get_complexity_score(image)
             f.write(image_name + ' ' + str(complexity) + '\n')
-            
+
+
 def compute_VGG16_Savoias():
     main_read_path = './Savoias-Dataset-master/images/'
     write_path = './VGG-16/test_results/new_Savoias_VGG16_fourth_layer_normalized.txt'
@@ -51,14 +53,13 @@ def compute_VGG16_Savoias():
             for image_name in os.listdir(read_path):
                 print(f"Processing image {i}...", end="\r")
                 i+=1      
-                image = Image.open(read_path + image_name)
+                image = Image.open(read_path + image_name).convert('RGB')
                 output_image_name = category + '_' + image_name
-                if(image.mode != 'RGB'):
-                    image = image.convert('RGB')
                 input_tensor = preprocess(image)
                 complexity_score = get_complexity_score(input_tensor)
                 f.write(output_image_name + ' ' + str(complexity_score) + '\n')
-                
+
+ 
 if __name__ == '__main__':
     '''IC9600'''
     compute_VGG16_IC9600()
